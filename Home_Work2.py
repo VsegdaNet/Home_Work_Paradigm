a7a80b8c5f7b85f804b35a6badd451f2c8827944
anna <ee@ss.e>
2023-12-03 00:08:45 -0800

    Home_Work

---
+++ Home_Work2.py
@@ -0,0 +1,1 @@
+# Я выбрал Императивный подход потому что я могу управлять так как мне надо# состоянием программыdef sum(num):    for i in range(1, num + 1):        for j in range(1, 11):            print(f"{i} * {j} = {i * j}", end=' || ')        print('\n')if __name__ == "__main__":    sum(8)
