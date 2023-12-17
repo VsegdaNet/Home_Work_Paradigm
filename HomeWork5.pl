25ac888a0543d33a67f9b1e2bdd57057abd18570
anna <ee@ss.e>
2023-12-17 13:09:37 -0800

    Home_Work

--- HomeWork5.pl
+++ HomeWork5.pl
@@ -1,1 +1,7 @@
-sum_list([], 0).sum_list([Head|Tail], Sum) :-    sum_list(Tail, TailSum),    Sum is Head + TailSum.?- sum_list([1, 2, 3, 4, 5], X).X = 15.
+sum_list([], 0).
+sum_list([Head|Tail], Sum) :-
+    sum_list(Tail, TailSum),
+    Sum is Head + TailSum.
+
+?- sum_list([1, 2, 3, 4, 5], X).
+X = 15.
